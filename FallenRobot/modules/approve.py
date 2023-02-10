e6db143b2aac29bc4965e092867a883038735aec
RN_ny <ronnyagila1995@gmail.com>
2023-02-10 13:43:30 -0500

    Update approve.py

--- FallenRobot/modules/approve.py
+++ FallenRobot/modules/approve.py
@@ -198,17 +198,17 @@ def unapproveall_btn(update: Update, context: CallbackContext):
 
 
 __help__ = """
-Sometimes, you might trust a user not to send unwanted content.
-Maybe not enough to make them admin, but you might be ok with locks, blacklists, and antiflood not applying to them.
+A veces, puede confiar en que un usuario no envíe contenido no deseado.
+Tal vez no sea suficiente para convertirlos en administradores, pero es posible que esté de acuerdo con que los bloqueos, las listas negras y la protección contra inundaciones no se apliquen a ellos.
 
-That's what approvals are for - approve of trustworthy users to allow them to send 
+Para eso están las aprobaciones - aprueba a usuarios confiables para permitirles enviar. 
 
 *Admin commands:*
-❍ /approval*:* Check a user's approval status in this chat.
-❍ /approve*:* Approve of a user. Locks, blacklists, and antiflood won't apply to them anymore.
-❍ /unapprove*:* Unapprove of a user. They will now be subject to locks, blacklists, and antiflood again.
-❍ /approved*:* List all approved users.
-❍ /unapproveall*:* Unapprove *ALL* users in a chat. This cannot be undone.
+❍ /approval*:* Consultar el estado de aprobación de un usuario en este chat.
+❍ /approve*:* Aprueba a un usuario. Los bloqueos, las listas negras y la protección contra inundaciones ya no se aplicarán a ellos.
+❍ /unapprove*:* Desaprueba a un usuario. Ahora estarán sujetos a bloqueos, listas negras y antiinundaciones nuevamente.
+❍ /approved*:* Lista de todos los usuarios aprobados.
+❍ /unapproveall*:* Desaprueba *TODOS* los usuarios en un chat. Esto no se puede deshacer.
 """
 
 APPROVE = DisableAbleCommandHandler("approve", approve)
