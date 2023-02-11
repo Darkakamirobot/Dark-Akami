3e13fbb0e549bdc0e6f0734a800bc211cdae868e
RN_ny <ronnyagila1995@gmail.com>
2023-02-11 14:09:14 -0500

    Update zombies.py

--- FallenRobot/modules/zombies.py
+++ FallenRobot/modules/zombies.py
@@ -11,13 +11,13 @@ from FallenRobot.utils.admins import can_restrict
 @can_restrict
 async def ban_zombies(_, message: Message):
     del_zom = 0
-    no_z = "`0 deleted accounts found in this chat.`"
+    no_z = "`0 cuentas eliminadas encontradas en este chat.`"
     try:
         clean = message.text.split(None, 1)[1]
     except:
         clean = None
     if clean != "clean":
-        check = await message.reply_text("`Searching for deleted accounts...`")
+        check = await message.reply_text("`Buscando cuentas eliminadas...`")
         async for user in pbot.get_chat_members(message.chat.id):
             if user.user.is_deleted:
                 del_zom += 1
@@ -28,7 +28,7 @@ async def ban_zombies(_, message: Message):
             )
         else:
             return await check.edit_text(no_z)
-    cleaner = await message.reply_text("`Cleaning deleted accounts from this chat...`")
+    cleaner = await message.reply_text("`Limpieza de cuentas eliminadas de este chat...`")
     deleted_u = []
     banned = 0
     failed = 0
