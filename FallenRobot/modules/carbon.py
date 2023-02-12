fe95f60f1888be00147f37040bf85d7003e96095
RN_ny <ronnyagila1995@gmail.com>
2023-02-12 14:23:11 -0500

    Update carbon.py

--- FallenRobot/modules/carbon.py
+++ FallenRobot/modules/carbon.py
@@ -9,12 +9,12 @@ from FallenRobot.utils.functions import make_carbon
 @capture_err
 async def carbon_func(_, message):
     if not message.reply_to_message:
-        return await message.reply_text("ʀᴇᴩʟʏ ᴛᴏ ᴀ ᴛᴇxᴛ ᴛᴏ ɢᴇɴᴇʀᴀᴛᴇ ᴄᴀʀʙᴏɴ.")
+        return await message.reply_text("ʀᴇsᴘᴏɴᴅᴇ ᴀ ᴜɴ ᴛᴇxᴛᴏ ᴘᴀʀᴀ ɢᴇɴᴇʀᴀʀ ᴄᴀʀʙᴏɴ.")
     if not message.reply_to_message.text:
-        return await message.reply_text("ʀᴇᴩʟʏ ᴛᴏ ᴀ ᴛᴇxᴛ ᴛᴏ ɢᴇɴᴇʀᴀᴛᴇ ᴄᴀʀʙᴏɴ.")
-    m = await message.reply_text("ɢᴇɴᴇʀᴀᴛɪɴɢ ᴄᴀʀʙᴏɴ...")
+        return await message.reply_text("ʀᴇsᴘᴏɴᴅᴇ ᴀ ᴜɴ ᴛᴇxᴛᴏ ᴘᴀʀᴀ ɢᴇɴᴇʀᴀʀ ᴄᴀʀʙᴏɴ.")
+    m = await message.reply_text("ɢᴇɴᴇʀᴀɴᴅᴏ ᴄᴀʀʙᴏɴ...")
     carbon = await make_carbon(message.reply_to_message.text)
-    await m.edit_text("ᴜᴩʟᴏᴀᴅɪɴɢ ɢᴇɴᴇʀᴀᴛᴇᴅ ᴄᴀʀʙᴏɴ...")
+    await m.edit_text("ᴄᴀʀɢᴀɴᴅᴏ ᴄᴀʀʙᴏɴ ɢᴇɴᴇʀᴀᴅᴏ...")
     await pbot.send_photo(message.chat.id, carbon)
     await m.delete()
     carbon.close()
