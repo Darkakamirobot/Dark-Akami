4b5f001630f109a903256eb9763ecf1a698b87ba
RN_ny <ronnyagila1995@gmail.com>
2023-02-16 11:36:28 -0500

    Update group.py

--- FallenRobot/modules/group.py
+++ FallenRobot/modules/group.py
@@ -1,9 +1,9 @@
 __help__ = """
- ❍ /setgtitle <newtitle>*:* Establece un nuevo título de chat en tu grupo.
+ ❍ /setgtitle <nuevo título>*:* Establece un nuevo título de chat en tu grupo.
  ❍ /setgpic*:* Como respuesta a un archivo o foto para establecer una foto de perfil de grupo!
  ❍ /delgpic*:* Igual que arriba pero para eliminar la foto de perfil del grupo.
  ❍ /setsticker*:* Como respuesta a algun sticker para configurarla como conjunto de stickers grupales!
- ❍ /setdescription <description>*:* Establece una nueva descripción de chat en grupo.
+ ❍ /setdescription <descripción>*:* Establece una nueva descripción de chat en grupo.
 """
 
 __mod_name__ = "Gʀᴏᴜᴘ"
